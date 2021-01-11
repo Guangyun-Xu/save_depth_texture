b69ec73b7a997e86811cc43836acfaa7979ca345
Guangyun-Xu <785618777@qq.com>
2021-01-11 23:48:22 +0800

    make save() as a individual function

--- main.cpp
+++ main.cpp
@@ -21,6 +21,11 @@
 pho::api::PFrame get_frame(const pho::api::PPhoXi &phoXi_device);
 int coustm_save(const pho::api::PPhoXi &phoXi_device,
                 const std::string &save_dir);
+int squence_save(const pho::api::PPhoXi &phoXi_device,
+                 const std::string &save_dir);
+int save(const pho::api::PFrame &frame,
+         const std::string &depth_path,
+         const std::string &texture_path);
 
 
 int main()
@@ -84,44 +89,61 @@ pho::api::PFrame get_frame(const pho::api::PPhoXi &phoXi_device)
 int coustm_save(const pho::api::PPhoXi &phoXi_device,
                 const std::string &save_dir)
 {
-   pho::api::PFrame frame = get_frame(phoXi_device);
-
    if (access(save_dir.c_str(), 0) == -1)
        mkdir(save_dir.c_str(), S_IRWXU);
 
-   float *texture_data = new float[frame->Texture.Size.Area()];
-   float *depth_data = new float[frame->DepthMap.Size.Area()];
-
-   int height = frame->DepthMap.Size.Height;
-   int width = frame->DepthMap.Size.Width;
-
-   frame->Texture.ConvertTo2DArray(texture_data, height, width);
-   frame->DepthMap.ConvertTo2DArray(depth_data, height, width);
-
-   cv::Mat depth_mat(cv::Size(width, height), CV_32FC1, depth_data);
-   depth_mat = depth_mat * 10.0f;  // conver the unit of depth to 0.1mm
-   depth_mat.convertTo(depth_mat, CV_16UC1);
-   cv::Mat texture_mat(cv::Size(width, height), CV_32FC1, texture_data);
-
-   float crop_rate = (1.0f - (671.0f/1032.0f))/2.0f;
-   int width_top = int(width*crop_rate);
-   int height_down = int(height*crop_rate);
-   cv::Rect crop_roi(width_top, height_down, 671, 502);
-
-   cv::Mat cropped_depth = depth_mat(crop_roi);
-   cv::Mat cropped_texture = texture_mat(crop_roi);
-
-   // normalize texture image
-   cv::normalize(cropped_texture, cropped_texture, 0, 450, cv::NORM_MINMAX);
+   pho::api::PFrame frame = get_frame(phoXi_device);
 
    std::string depth_path = save_dir + "/depth.png";
    std::string texture_path = save_dir + "/texture.png";
 
-   cv::imwrite(depth_path, cropped_depth);
-   cv::imwrite(texture_path, cropped_texture);
-
-   std::cout << "depth saved in: " << depth_path <<std::endl;
-   std::cout << "texture saved in:" << texture_path << std::endl;
+   save(frame, depth_path, texture_path);
 
    return 0;
+}
+
+int squence_save(const pho::api::PPhoXi &phoXi_device,
+                 const std::string &save_dir)
+{
+
+}
+
+int save(const pho::api::PFrame &frame,
+         const std::string &depth_path,
+         const std::string &texture_path)
+{
+    float *texture_data = new float[frame->Texture.Size.Area()];
+    float *depth_data = new float[frame->DepthMap.Size.Area()];
+
+    int height = frame->DepthMap.Size.Height;
+    int width = frame->DepthMap.Size.Width;
+
+    frame->Texture.ConvertTo2DArray(texture_data, height, width);
+    frame->DepthMap.ConvertTo2DArray(depth_data, height, width);
+
+    cv::Mat depth_mat(cv::Size(width, height), CV_32FC1, depth_data);
+    depth_mat = depth_mat * 10.0f;  // conver the unit of depth to 0.1mm
+    depth_mat.convertTo(depth_mat, CV_16UC1);
+    cv::Mat texture_mat(cv::Size(width, height), CV_32FC1, texture_data);
+
+    float crop_rate = (1.0f - (671.0f/1032.0f))/2.0f;
+    int width_top = int(width*crop_rate);
+    int height_down = int(height*crop_rate);
+    cv::Rect crop_roi(width_top, height_down, 671, 502);
+
+    cv::Mat cropped_depth = depth_mat(crop_roi);
+    cv::Mat cropped_texture = texture_mat(crop_roi);
+
+    // normalize texture image
+    cv::normalize(cropped_texture, cropped_texture, 0, 450, cv::NORM_MINMAX);
+
+
+    cv::imwrite(depth_path, cropped_depth);
+    cv::imwrite(texture_path, cropped_texture);
+
+    std::cout << "depth saved in: " << depth_path <<std::endl;
+    std::cout << "texture saved in:" << texture_path << std::endl;
+
+    return 0;
+
 }
